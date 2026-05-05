dc544ace20fd45fe7ffde0998bf606c3f46b6d15
Andrade23 <fabkevandrade23@gmail.com>
2026-05-05 21:50:42 +0200

    Update ia.js

--- Frontend/Javascript/ia.js
+++ Frontend/Javascript/ia.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 async function runAI() {
 
   const tasks = ["Task 1", "Task 2"];
@@ -6,24 +5,10 @@ async function runAI() {
   const res = await fetch("http://localhost:8000/ai/prioritize", {
     method: "POST",
     headers: {"Content-Type": "application/json"},
-=======
-async function openAI() {
-  const tasks = [...document.querySelectorAll(".task")]
-    .map(t => t.innerText);
-
-  const res = await fetch("/ai/prioritize", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
->>>>>>> ea5a9878f07903e96f59f6dd44012f742124cbef
     body: JSON.stringify({ tasks })
   });
 
   const data = await res.json();
-<<<<<<< HEAD
 
-=======
->>>>>>> ea5a9878f07903e96f59f6dd44012f742124cbef
   alert(data.result);
 }
