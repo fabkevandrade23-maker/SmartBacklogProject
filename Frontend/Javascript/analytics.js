2b0553c53c8a646900deb02fdba5ce64e14a7ff4
Andrade23 <fabkevandrade23@gmail.com>
2026-05-05 21:46:12 +0200

    Update analytics.js

--- Frontend/Javascript/analytics.js
+++ Frontend/Javascript/analytics.js
@@ -5,20 +5,3 @@ async function loadAnalytics() {
 
   console.log(data);
 }
-=======
-  const res = await fetch("/analytics");
-  const data = await res.json();
-
-  new Chart(document.getElementById("chart"), {
-    type: "bar",
-    data: {
-      labels: ["To Do", "Progress", "Done"],
-      datasets: [{
-        data: [data.todo, data.progress, data.done]
-      }]
-    }
-  });
-}
-
-loadAnalytics();
->>>>>>> ea5a9878f07903e96f59f6dd44012f742124cbef
